e9f6a10c38960094b502304dcdc190953e704e72
russellmilne <russell.milne@mail.mcgill.ca>
2019-08-06 15:57:06 -0400

    More syntax
    "getElementsById" isn't valid since ids are supposed to be unique, so my second button wasn't doing anything

--- testscript.js
+++ testscript.js
@@ -3,6 +3,7 @@ function weirdtestfunction() {
 }
 
 function othertestfunction() {
-	document.getElementsById("testground").innerHTML = "I changed a paragraph, or maybe several"
+	document.getElementsByTagName("p").innerHTML = "Let's see if this alters the dropdown menu."
+	document.getElementById("testground").innerHTML = "I changed a paragraph, or maybe several"
 	document.getElementsByTagName("button").innerHTML = "Also a couple buttons"
 }
